7a60944d4f59f950d10c3bed89a5db214e3a4da2
adamski201 <90043609+adamski201@users.noreply.github.com>
2022-11-17 14:50:59 +0000

    added fire functionality to furnace

---
+++ ChemistryGame/Assets/Scripts/CountCoal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CountCoal : MonoBehaviour
+{
+    public XRSocketInteractor socket1;
+    public XRSocketInteractor socket2;
+    public XRSocketInteractor socket3;
+    public GameObject fire;
+
+    private void Start()
+    {
+        fire.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (FilledWithCoal())
+        {
+            fire.SetActive(true);
+        } else
+        {
+            fire.SetActive(false);
+        }
+    }
+
+   private bool FilledWithCoal()
+    {
+        Debug.Log(socket1.hasSelection && socket2.hasSelection && socket3.hasSelection);
+        return socket1.hasSelection && socket2.hasSelection && socket3.hasSelection;
+    }
+}
