f6284cfa5cef874d5eb2e0dd849b303204000886
adamski201 <90043609+adamski201@users.noreply.github.com>
2022-12-01 13:18:22 +0000

    added new fire functionality

--- ChemistryGame/Assets/Scripts/LightFire.cs
+++ ChemistryGame/Assets/Scripts/LightFire.cs
@@ -8,26 +8,61 @@ public class LightFire : MonoBehaviour
     public XRSocketInteractor socket1;
     public XRSocketInteractor socket2;
     public XRSocketInteractor socket3;
-    public GameObject fire;
+    public GameObject weakFire;
+    public GameObject mediumFire;
+    public GameObject strongFire;
+    private int lastCoalCount = 0;
 
     private void Awake()
     {
-        fire.SetActive(false);
+        weakFire.SetActive(false);
+        mediumFire.SetActive(false);
+        strongFire.SetActive(false);
     }
 
     private void Update()
     {
-        if (FilledWithCoal())
-        {
-            fire.SetActive(true);
-        } else
-        {
-            fire.SetActive(false);
-        }
+        HandleFire();
     }
 
-   private bool FilledWithCoal()
+    private int CountCoal()
     {
-        return socket1.hasSelection && socket2.hasSelection && socket3.hasSelection;
+        return (socket1.hasSelection ? 1 : 0) + (socket2.hasSelection ? 1 : 0) + (socket3.hasSelection ? 1 : 0);
+    }
+
+    private void HandleFire()
+    {
+        int fireStrength = CountCoal();
+
+        if (fireStrength != lastCoalCount)
+        {
+            switch (fireStrength)
+            {
+                case 1:
+                    weakFire.SetActive(true);
+                    mediumFire.SetActive(false);
+                    strongFire.SetActive(false);
+                    lastCoalCount = 1;
+                    break;
+                case 2:
+                    mediumFire.SetActive(true);
+                    weakFire.SetActive(false);
+                    strongFire.SetActive(false);
+                    lastCoalCount = 2;
+                    break;
+                case 3:
+                    strongFire.SetActive(true);
+                    weakFire.SetActive(false);
+                    mediumFire.SetActive(false);
+                    lastCoalCount = 3;
+                    break;
+                default:
+                    weakFire.SetActive(false);
+                    mediumFire.SetActive(false);
+                    strongFire.SetActive(false);
+                    lastCoalCount = 0;
+                    break;
+            }
+        }
     }
 }
