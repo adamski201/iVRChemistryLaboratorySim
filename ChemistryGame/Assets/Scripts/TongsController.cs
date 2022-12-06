dd081a6777464fc9adcd481a49ba9563a5c91614
adamski201 <90043609+adamski201@users.noreply.github.com>
2022-12-06 14:52:11 +0000

    implemented new tongs functionality

---
+++ ChemistryGame/Assets/Scripts/TongsController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TongsController : MonoBehaviour
+{
+    [SerializeField] private XRSocketInteractor tongSocket;
+    private XRGrabInteractable coalObj;
+
+    private void Start()
+    {
+        tongSocket.socketActive = false;
+
+        tongSocket.hoverEntered.AddListener(OnHoverEnter);
+        tongSocket.hoverExited.AddListener(OnHoverExit);
+    }
+
+    public void PickUpCoal()
+    {
+        tongSocket.socketActive = true;
+        if (coalObj != null)
+        {
+            tongSocket.StartManualInteraction(coalObj);
+        }
+
+        XRSocketInteractor coalHolder = coalObj.selectingInteractor.GetComponent<XRSocketInteractor>();
+        if (coalHolder != null)
+        {
+            StartCoroutine(DisableSocket(coalHolder));
+            tongSocket.StartManualInteraction(coalObj);
+        }
+    }
+
+    public void DropCoal()
+    {
+        if (tongSocket.hasSelection)
+        {
+            tongSocket.EndManualInteraction();
+        }
+
+        tongSocket.socketActive = false;
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        coalObj = args.interactableObject.transform.GetComponent<XRGrabInteractable>();
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args)
+    {
+        coalObj = null;
+    }
+
+    IEnumerator DisableSocket(XRSocketInteractor socket)
+    {
+        socket.socketActive = false;
+
+        yield return new WaitForSeconds(1);
+
+        socket.socketActive = true;
+
+        yield return null;
+    }
+}
